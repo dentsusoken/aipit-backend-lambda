de69baf4462bc7264b0a8774b0c44e464098819c
fcf-koga <k-koga@future-creation-f.co.jp>
2025-05-11 09:12:49 +0000

    pre-commitの導入

--- src/hello_world/app.py
+++ src/hello_world/app.py
@@ -1,28 +1,30 @@
 import json
+from typing import Any, Dict
 
 # import requests
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     """Sample pure Lambda function
 
     Parameters
     ----------
     event: dict, required
         API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/
+                  developerguide/set-up-lambda-proxy-integrations.html
+                  #api-gateway-simple-proxy-for-lambda-input-format
 
     context: object, required
         Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+        Context doc: https://docs.aws.amazon.com/lambda/latest/
+                     dg/python-context-object.html
 
     Returns
     ------
     API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/
+                    developerguide/set-up-lambda-proxy-integrations.html
     """
 
     # try:
