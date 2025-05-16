81207d20f6c2d880d0f6cc58c13e25298dd49ea1
masaya fukuhara <xfukuhara.masaya@contract.dentsusoken.com>
2025-05-16 04:46:42 +0000

    [move] types をsrc/hello_world/modules に変更した

--- src/hello_world/app.py
+++ src/hello_world/app.py
@@ -1,4 +1,4 @@
-from types.lambda_events import LambdaResponse
+from modules.lambda_events import LambdaResponse
 
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import APIGatewayProxyEventV2
