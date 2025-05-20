e26f4a96871352abc4d9ad4eaf1e03f7ef26723c
masaya fukuhara <xfukuhara.masaya@contract.dentsusoken.com>
2025-05-20 06:54:04 +0000

    [refactor] S3 ラムダのファイルをまとめた

--- src/S3/app.py
+++ src/S3/app.py
@@ -1,22 +1,31 @@
 import json
 import logging
 
+import boto3
+from botocore.exceptions import ClientError
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import APIGatewayProxyEventV2
 
 from modules.lambda_events import LambdaResponse
-from create_s3_resource import create_s3_resource
-from get_response import get_response
-from put_object import put_object
+from modules.get_response import get_response
+from modules.constants import (
+    AWS_DEFAULT_REGION,
+    AWS_ENDPOINT_URL,
+    BUCKET_NAME,
+    OBJECT_NAME,
+)
 
 
 def lambda_handler(event: APIGatewayProxyEventV2, context: Context) -> LambdaResponse:
     logging.info('start')
-    s3_resource = create_s3_resource()
 
-    if s3_resource is None:
+    try:
+        s3_resource = boto3.resource(
+            's3', region_name=AWS_DEFAULT_REGION, endpoint_url=AWS_ENDPOINT_URL)
+
+    except ClientError as e:
         message = 'Can\'t create resource'
-        logging.error(message)
+        logging.error(e)
 
         return get_response(500, message)
 
@@ -24,8 +33,8 @@ def lambda_handler(event: APIGatewayProxyEventV2, context: Context) -> LambdaRes
 
     try:
         body = json.loads(event['body'])
-        bucket_name = body['bucket_name'] if 'bucket_name' in body else None
-        object_name = body['object_name'] if 'object_name' in body else None
+        bucket_name = body['bucket_name'] if 'bucket_name' in body else BUCKET_NAME
+        object_name = body['object_name'] if 'object_name' in body else OBJECT_NAME
 
     except Exception:
         message = 'The body must be in JSON format.'
@@ -34,16 +43,14 @@ def lambda_handler(event: APIGatewayProxyEventV2, context: Context) -> LambdaRes
         return get_response(400, message)
 
     json_obj = {'message': 'This is sample'}
-    result = put_object(
-        s3_resource,
-        json_obj,
-        bucket_name=bucket_name,
-        object_name=object_name
-    )
 
-    if result is False:
+    try:
+        s3_obj = s3_resource.Object(bucket_name, object_name)
+        s3_obj.put(Body=json.dumps(json_obj))
+
+    except ClientError as e:
         message = 'Can\'t put a object'
-        logging.error(message)
+        logging.error(e)
 
         return get_response(500, message)
 
