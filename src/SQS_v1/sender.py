a27d663aaa5c6426872d0175ab60df3cde4cb07d
masaya fukuhara <xfukuhara.masaya@contract.dentsusoken.com>
2025-06-04 02:00:44 +0000

    [feature] SQS にMetrics を追加した

--- src/SQS_v1/sender.py
+++ src/SQS_v1/sender.py
@@ -1,8 +1,10 @@
 import json
 import os
+import time
 
 import boto3
-from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Logger, Metrics
+from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import APIGatewayProxyEventV1
 from aws_lambda_typing.responses import APIGatewayProxyResponseV1
@@ -12,11 +14,13 @@ AWS_ENDPOINT_URL = os.environ.get("AWS_ENDPOINT_URL", None)
 QUEUE_NAME = os.environ.get("QUEUE_NAME", "sampleQueue.fifo")
 sqs = boto3.client("sqs", region_name=AWS_DEFAULT_REGION, endpoint_url=AWS_ENDPOINT_URL)
 logger = Logger(service="HelloWorldService")
+metrics = Metrics(namespace="SQSSenderFunction", service="SQSSender")
 
 
 def lambda_handler(
     event: APIGatewayProxyEventV1, context: Context
 ) -> APIGatewayProxyResponseV1:
+    start_time = time.time()
 
     try:
         body = json.loads(event["body"]) if event["body"] is not None else {}
@@ -37,6 +41,12 @@ def lambda_handler(
             QueueUrl=queue_url, MessageBody=messageBody, MessageGroupId="sampleSQS"
         )
 
+        end_time = time.time()
+        duration_ms = (end_time - start_time) * 1000
+
+        metrics.add_metric(
+            name="ProcessingTime", unit=MetricUnit.Milliseconds, value=duration_ms
+        )
         return {
             "statusCode": 200,
             "body": json.dumps({"message": f"sent {send_response['MessageId']}"}),
