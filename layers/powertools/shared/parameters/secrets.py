bb284b3a9a85bfd222cc31ef9c3ae13b256e7a88
masaya fukuhara <xfukuhara.masaya@contract.dentsusoken.com>
2025-06-18 07:09:32 +0000

    [update] データ取得をparameter store からシークレットマネージャーにした

--- layers/powertools/shared/parameters/secrets.py
+++ layers/powertools/shared/parameters/secrets.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from aws_lambda_powertools.utilities.parameters import SecretsProvider
-from shared.parameters.utils import raw_to_obj
+from shared.parameters.utils import raw_to_obj, raw_to_str
 
 
 class Secrets:
@@ -22,3 +22,18 @@ class Secrets:
         raw = self.secrets.get(key)
 
         return raw_to_obj(raw)
+
+    def get_secret_str(self, key: str) -> str:
+        """
+        指定されたキーに対応するシークレットを SecretsProvider から取得し、文字列として返します。
+
+        引数:
+            key (str): 取得したいシークレットのキー。
+
+        戻り値:
+            Any: 取得されたシークレットの文字列。
+        """
+
+        raw = self.secrets.get(key)
+
+        return raw_to_str(raw)
